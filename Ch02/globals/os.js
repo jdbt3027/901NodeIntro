04c935172593b34142d4afbd4476351e3cd5f542
jdbt3027 <thetimmons@verizon.net>
2019-07-19 10:12:24 -0400

    Remove duplicated directory

--- Ch02/globals/os.js
+++
@@ -1,4 +0,0 @@
-const os = require('os');
-
-console.log(`Total Memory: ${os.totalmem()}  Free Memory: ${os.freemem()}`);
-
--- Ch02/modules/call-band.js
+++
@@ -1,6 +0,0 @@
-const Band = require('./constructor-band');
-
-const beatles = new Band('The Beatles', ['John', 'Paul', 'George', 'Ringo']);
-
-console.log(`beatles member count`, beatles.memberCount());
-console.log(`has Ringo?`, beatles.hasMember('Ringo'));
